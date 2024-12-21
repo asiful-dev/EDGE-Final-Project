faa86c77148e733f75e95ad57e71ec692fedea3a
AwfulAsiful <awful872@gmail.com>
2024-12-21 12:49:37 +0600

    project setup done ✅  and  created a navbar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+function App() {
+
+
+  return (
+    <>
+    <Navbar></Navbar>
+     <h2>Fuck off world!!!
+     </h2>
+    </>
+  )
+}
+
+export default App
