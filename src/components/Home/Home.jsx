11c1b5dbc934087e8e85b68a05c87a43c9875333
AwfulAsiful <awful872@gmail.com>
2024-12-23 11:00:35 +0600

    Home Page Component added

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+import Banner from '../Banner/Banner'
+import JokeCard from '../JokeCard/JokeCard'
+
+function Home() {
+  return (
+    <>
+    <Navbar/>
+    <Banner/>
+    <JokeCard/>
+    </>
+  )
+}
+
+export default Home
